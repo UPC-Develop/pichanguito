50008804e91a9d005022fac2296918c374f36b55
jcajasba <joel.cajas@telefonica.com>
2022-05-02 13:18:54 -0500

    Se agrego el menu de opciones

--- pichanguito/app/src/main/java/com/upc/pichanguito/HomeActivity.java
+++ pichanguito/app/src/main/java/com/upc/pichanguito/HomeActivity.java
@@ -1,9 +1,12 @@
 package com.upc.pichanguito;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,28 +15,39 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button signoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        signoutButton = findViewById(R.id.signoutButton);
-        signoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Toast.makeText(HomeActivity.this, "Cerrando sesión...", Toast.LENGTH_SHORT).show();
-                setSignIn();
-            }
-        });
     }
 
-    private void setSignIn(){
+    private void SignOut(){
 
+        FirebaseAuth.getInstance().signOut();
+        Toast.makeText(HomeActivity.this, "Cerrando sesión...", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.signOutMenu:
+                SignOut();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
